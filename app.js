fc59347f8bf285018d547232ede45ddba23f4a27
carlos <carlos.20499@gmail.com>
2020-05-15 20:46:24 -0400

    agregado morgan cors y path

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('holaa');
-});
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(4000, function () {
-  console.log('escunchando puerto 4000');
+//rutas
+// app.get('/', function (req, res) {
+//   res.send('holaa');
+// });
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('puerto', process.env.PORT || 4000);
+
+app.listen(app.get('puerto'), function () {
+  console.log('escunchando puerto: ', app.get('puerto'));
 });
